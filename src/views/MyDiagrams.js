f6de3a2ecbe2af6717860b89b5d320446d7c3991
Fernando <fmontero04@gmail.com>
2018-06-10 13:51:36 -0400

    Confirmado

--- src/views/MyDiagrams.js
+++ src/views/MyDiagrams.js
@@ -1,13 +1,22 @@
 import React from 'react'
-
+import DiagramCard from '../components/DiagramCard'
+import {mydiagrams} from '../request/mydiagrams'
 class MyDiagrams extends React.Component {
     constructor(props){
         super(props)
     }
 
+    renderDiagrams(){
+        return mydiagrams.map((diagram, index)=>{
+            return <DiagramCard name={diagram.name} date={diagram.date} description={diagram.description} image={diagram.image}/> 
+        })    
+    }
+
     render(){
         return (
-            <label>My Diagrams</label>
+            <div className="row">
+                {this.renderDiagrams()}
+            </div>
         )
     }
 }
